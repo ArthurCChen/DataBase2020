0c32e6c8efb7d176e4362c11171586291fc5b105
chuchong <33688310+chuchong@users.noreply.github.com>
2020-04-29 12:18:14 +0800

    feature serialization NOTICE: table will only be serialize as the whole

--- src/main/java/cn/edu/thssdb/schema/Table.java
+++ src/main/java/cn/edu/thssdb/schema/Table.java
@@ -1,8 +1,11 @@
 package cn.edu.thssdb.schema;
 
 import cn.edu.thssdb.index.BPlusTree;
+import cn.edu.thssdb.index.BPlusTreeIterator;
+import cn.edu.thssdb.utils.Global;
 import javafx.util.Pair;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -35,13 +38,27 @@ public class Table implements Iterable<Row> {
     // TODO
   }
 
-  private void serialize() {
-    // TODO
+  private void serialize(String filename) throws IOException {
+    // TODO 改进暂时输出当前table所有row的不足
+    FileOutputStream fileOut = new FileOutputStream(filename);
+    ObjectOutputStream ooStream = new ObjectOutputStream(fileOut);
+    ArrayList<Row> rows = new ArrayList<>();
+    BPlusTreeIterator<Entry, Row> iterator = index.iterator();
+    while (iterator.hasNext()) {
+      ooStream.writeObject(iterator.next());
+    }
+    ooStream.close();
   }
 
-  private ArrayList<Row> deserialize() {
+  private ArrayList<Row> deserialize(String filename) throws IOException, ClassNotFoundException, ClassCastException{
     // TODO
-    return null;
+    FileInputStream fileIn = new FileInputStream(filename);
+    ObjectInputStream oiStream = new ObjectInputStream(fileIn);
+    Object obj = oiStream.readObject();
+    ArrayList<Row> rows = null;
+    rows = (ArrayList<Row>) Global.castList(obj, Row.class);
+    oiStream.close();
+    return rows;
   }
 
   private class TableIterator implements Iterator<Row> {
--- src/main/java/cn/edu/thssdb/utils/Global.java
+++ src/main/java/cn/edu/thssdb/utils/Global.java
@@ -1,5 +1,8 @@
 package cn.edu.thssdb.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Global {
   public static int fanout = 129;
 
@@ -14,4 +17,20 @@ public class Global {
   public static final String QUIT = "quit;";
 
   public static final String S_URL_INTERNAL = "jdbc:default:connection";
+
+  //用于转换Object为数组类型
+  //https://www.cnblogs.com/xingmangdieyi110/p/11676553.html
+  public static <T> List<T> castList(Object obj, Class<T> clazz) throws ClassCastException
+  {
+    List<T> result = new ArrayList<T>();
+    if(obj instanceof List<?>)
+    {
+      for (Object o : (List<?>) obj)
+      {
+        result.add(clazz.cast(o));
+      }
+      return result;
+    }
+    return null;
+  }
 }
