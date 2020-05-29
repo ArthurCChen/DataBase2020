9efa227317dfd30a6ecbc82500b26ae2b9dbb01f
tgxs002 <38932123+tgxs002@users.noreply.github.com>
2020-05-30 00:47:52 +0800

    start implementing test tools

---
+++ src/main/java/cn/edu/thssdb/memory_db/MDBManager.java
@@ -0,0 +1,68 @@
+package cn.edu.thssdb.memory_db;
+
+import cn.edu.thssdb.schema.Column;
+import cn.edu.thssdb.schema.Entry;
+import cn.edu.thssdb.schema.Row;
+import cn.edu.thssdb.schema.Table;
+import cn.edu.thssdb.utils.Physical2LogicalInterface;
+
+import java.util.ArrayList;
+
+/**
+ * This is a implementation of cn.edu.thssdb.utils.Physical2LogicalInterface,
+ * It is for logical layer's test only.
+ * Please do not call methods of this class from physical layer.
+ *
+ * This class stands alone as an independent and minimal physical layer.
+ * It guarantees correctness, but the efficiency can be ugly.
+ *
+ * For physical layer developers, you can check correctness by comparing output with this implementation.
+ */
+public class MDBManager implements Physical2LogicalInterface {
+
+
+    @Override
+    public boolean create_table(String table_name, ArrayList<Column> columns, int transaction_id) {
+        return false;
+    }
+
+    @Override
+    public boolean drop_table(String table_name, int transaction_id) {
+        return false;
+    }
+
+    @Override
+    public Table get_table(String table_name, int transaction_id) {
+        return null;
+    }
+
+    @Override
+    public boolean insert_row(String table_name, Row row, int transaction_id) {
+        return false;
+    }
+
+    @Override
+    public boolean delete_row(String table_name, Entry primary_key, int transaction_id) {
+        return false;
+    }
+
+    @Override
+    public Row search(String table_name, Entry primary_key, int transaction_id) {
+        return null;
+    }
+
+    @Override
+    public int start_transaction() {
+        return 0;
+    }
+
+    @Override
+    public boolean abort(int transaction_id) {
+        return false;
+    }
+
+    @Override
+    public boolean commit(int transaction_id) {
+        return false;
+    }
+}
